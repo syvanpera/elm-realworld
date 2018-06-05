cf61c9f1adc586fba3bc5553d2122560eebc62b6
Tuomo Syvänperä <tuomo.syvanpera@gmail.com>
2018-06-05 09:58:57 +0300

    Add JS implementation for ports

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import { Main } from './Main.elm';
 
 const storedSession = localStorage.getItem('session');
+const app = Main.fullscreen(JSON.parse(storedSession) || null);
 
-Main.fullscreen(JSON.parse(storedSession) || null);
+app.ports.storeSession.subscribe(function(session) {
+  localStorage.setItem('session', JSON.stringify(session));
+  app.ports.onSessionChange.send(session);
+});
+
+window.addEventListener("storage", function(event) {
+  if (event.storageArea === localStorage && event.key === "session") {
+    app.ports.onSessionChange.send(JSON.parse(event.newValue));
+  }
+}, false);
